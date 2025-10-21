f25c87980cd69a220cb40f473aa764d0e5b9c6d4
SuccessfullyFailed <renzogerritzen@gmail.com>
2025-10-21 23:24:14 +0200

    rename tests

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,10 @@ mod text_match_result;
 mod text_match_result_u;
 mod text_matcher;
 mod text_matcher_u;
-mod text_matcher_source;
-mod text_matcher_source_u;
+mod text_predicate;
+mod text_predicate_u;
 
 pub use text_matcher_set::*;
 pub use text_match_result::*;
 pub use text_matcher::*;
-pub use text_matcher_source::*;
+pub use text_predicate::*;
--- src/text_predicate_u.rs
+++ src/text_predicate_u.rs
@@ -1,12 +1,12 @@
 #[cfg(test)]
 mod tests {
-	use crate::{MatchHit, TextPredicate};
+	use crate::{ MatchHit, TextPredicate };
 
 
 
 	/* ATOM IMPLEMENTATION TESTS */
 	#[test]
-	fn test_text_matcher_atom_char() {
+	fn test_text_predicate_atom_char() {
 		assert_eq!('x'.match_text("xaba").unwrap().length, 1); // Partial match
 		assert_eq!('a'.match_text("xaba"), None); // Full mismatch
 		assert_eq!('x'.match_text("xxaba").unwrap().length, 1); // Non-repeating match
@@ -14,7 +14,7 @@ mod tests {
 	}
 
 	#[test]
-	fn test_text_matcher_atom_str() {
+	fn test_text_predicate_atom_str() {
 		assert_eq!("x".match_text("xaba").unwrap().length, 1); // Partial match
 		assert_eq!("xaba".match_text("xaba").unwrap().length, 4); // Full match
 		assert_eq!("a".match_text("xaba"), None); // Full mismatch
@@ -23,7 +23,7 @@ mod tests {
 	}
 
 	#[test]
-	fn test_text_matcher_atom_string() {
+	fn test_text_predicate_atom_string() {
 		assert_eq!("x".to_string().match_text("xaba").unwrap().length, 1); // Partial match
 		assert_eq!("xaba".to_string().match_text("xaba").unwrap().length, 4); // Full match
 		assert_eq!("a".to_string().match_text("xaba"), None); // Full mismatch
@@ -32,7 +32,7 @@ mod tests {
 	}
 
 	#[test]
-	fn test_text_matcher_atom_fn() {
+	fn test_text_predicate_atom_fn() {
 		assert_eq!((|text:&str| if text == "xaba" { Some(MatchHit::new(3, text)) } else { None }).match_text("xaba").unwrap().length, 3);
 		assert_eq!((|text:&str| if text == "daba" { Some(MatchHit::new(3, text)) } else { None }).match_text("xaba"), None);
 		assert_eq!((|text:&str| if text == "daba" { Some(MatchHit::new(3, text)) } else { None }).match_text(""), None);
@@ -43,7 +43,7 @@ mod tests {
 	/* LIST IMPLEMENTATION TESTS */
 
 	#[test]
-	fn test_text_matcher_list_array() {
+	fn test_text_predicate_list_array() {
 		assert_eq!(["x", "ab"].match_text("xaba").unwrap().length, 3); // Partial match
 		assert_eq!(["x", "ab", "a"].match_text("xaba").unwrap().length, 4); // Full match
 		assert_eq!(["a", "ab"].match_text("xaba"), None); // Full mismatch
@@ -54,7 +54,7 @@ mod tests {
 	}
 
 	#[test]
-	fn test_text_matcher_list_vec() {
+	fn test_text_predicate_list_vec() {
 		assert_eq!(vec!["x", "ab"].match_text("xaba").unwrap().length, 3); // Partial match
 		assert_eq!(vec!["x", "ab", "a"].match_text("xaba").unwrap().length, 4); // Full match
 		assert_eq!(vec!["a", "ab"].match_text("xaba"), None); // Full mismatch
@@ -65,7 +65,7 @@ mod tests {
 	}
 
 	#[test]
-	fn test_text_matcher_list_tuple() {
+	fn test_text_predicate_list_tuple() {
 		assert_eq!(('x', "ab").match_text("xaba").unwrap().length, 3); // Partial match
 		assert_eq!(('x', "ab", 'a').match_text("xaba").unwrap().length, 4); // Full match
 		assert_eq!(('a', "ab").match_text("xaba"), None); // Full mismatch
@@ -81,7 +81,7 @@ mod tests {
 	/* MISCELLANEOUS IMPLEMENTATION TESTS */
 
 	#[test]
-	fn test_text_matcher_misc_range() {
+	fn test_text_predicate_misc_range() {
 		assert_eq!(("x".."b").match_text("xoba").unwrap().length, 3); // Partial lengthy match
 		assert_eq!(("x".."a").match_text("xoba").unwrap().length, 4); // Full match
 		assert_eq!(("b".."a").match_text("xoba"), None); // Full mismatch
